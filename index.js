2b2f79e3e012b678ddda83d659b375b19d917b2a
Tanuka Das <das.tanuka@yahoo.com>
2019-03-30 21:52:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ it should return "The line is currently empty."*/
 function currentLine(katzDeliLine){
   var lineNum = [];
   for (var i=0; i < katzDeliLine.length; i++){
-    lineNum.push(" " + [i + 1] + ". " + katzDeliLine[i])                           //push the index and current position in the empty arrray
+    lineNum.push(" " + [i + 1] + ". " + katzDeliLine[i])          //push the index and current position in the empty arrray
   }
-  if (katzDeliLine.length === 0){
+  if (katzDeliLine.length === 0){                                 //if the array is 
   return "The line is currently empty."
 }
 else{
