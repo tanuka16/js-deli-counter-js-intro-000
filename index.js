a0a206bd0a5648d7eb4a4cf3c3b8e224adda02e9
Tanuka Das <das.tanuka@yahoo.com>
2019-03-30 19:22:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*Build a function that a new customer will use when entering the deli.
+The function, takeANumber, should accept two paramaters: the current line of people,
+along with the new person's name. The function should return a welcome message including
+the new person's position in line, such as "Welcome, Ada. You are number 1 in line.".
+And don't go being too programmer-y and give them their index. These are normal people.
+If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
+*/
 var katzDeliLine = [];                              //create an empty array
 
 function takeANumber(katzDeliLine, newName){        //function accepts 2 argument 1- people on line
@@ -19,7 +26,7 @@ function nowServing(katzDeliLine){
   }
   else {
     var customer = katzDeliLine.shift();                   //else remove the customer from the front of the array/line
-    return "Currently serving " + customer + ".";          //concat 
+    return "Currently serving " + customer + ".";          //concat
   }
 }
 
