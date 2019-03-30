e53c546fc63575bc99931ab5852eb52975598f38
Tanuka Das <das.tanuka@yahoo.com>
2019-03-30 21:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ it's just an example of a variable that might be passed to it. If there is nobod
 it should return "The line is currently empty."*/
 
 function currentLine(katzDeliLine){
+  var lineNum
   for (var i=0; i < katzDeliLine.length; i++){
     katzDeliLine.push
   }
